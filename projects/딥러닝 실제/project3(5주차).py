b0c298d817f24022de94b156f3d0e1bdd26a1bb8
원형일 <74521084+ED-Won@users.noreply.github.com>
2022-05-25 16:23:22 +0900

    Add files via upload

---
+++ projects/딥러닝 실제/project3(5주차).py
@@ -0,0 +1,54 @@
+# 전가산기
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+  
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else: 
+        return 1
+    
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1 
+    
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+def full_adder(x1, x2, x3):
+    
+    s1 = XOR(x1, x2)
+    _sum = XOR(s1, x3)
+    
+    c1 = AND(s1, x3)
+    c2 = AND(x1, x2)
+    _carry = OR(c1, c2)
+    
+    return _sum, _carry
+
+
